4fd98c8427e87d2b1c82dfcc83b21e543050bdc9
Danyel_Mario <109354951+DanielsMario@users.noreply.github.com>
2024-01-29 23:07:21 -0300

    Add files via upload
    Write a program that checks (using if and else) whether a typed letter is “B” or “G”. According to the letter: B – boy, G – girl, Invalid gender.

---
+++ Atv_easy_if_and_else_03.py
@@ -0,0 +1,8 @@
+Sexy = input("Enter whether you are a Boy (B) or a Girl (G): ")
+
+if Sexy == "G" or Sexy == "g":
+    print("Is Girl!")
+elif Sexy == "B" or Sexy == "b":
+    print("Is Boy!")
+else:
+    print("name not encontred!")
