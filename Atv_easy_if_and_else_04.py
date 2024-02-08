1c26e694008196fab0b7043516a631d111f33bf4
Danyel_Mario <109354951+DanielsMario@users.noreply.github.com>
2024-02-08 10:32:16 -0300

    Add files via upload
    Write a program that checks (using if and else) whether a typed letter is a vowel or a consonant.

---
+++ Atv_easy_if_and_else_04.py
@@ -0,0 +1,14 @@
+letra = input("Digite a letra desejada para saber se é vogal ou consoante: ")
+
+if letra == "A" or letra == "a":
+    print("Consoante")
+elif letra == "E" or letra == "e":
+    print("Consoante")
+elif letra == "I" or letra == "i":
+    print("Consoante")
+elif letra == "U" or letra == "o":
+    print("Consoante")
+elif letra == "U" or letra == "u":
+    print("Consoante")
+else:
+    print("Vogal")
